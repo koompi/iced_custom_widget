594304805eee4a716e12edc17f40358eb5b9e503
veasnama <maveasnalearntosolve@gmail.com>
2021-02-02 16:23:31 +0700

    intergrate sound effects

--- examples/sound.rs
+++ examples/sound.rs
@@ -717,90 +717,89 @@ fn main() {
 
 mod PlaySound {
 
-    use sdl2::audio::{self, AudioCallback, AudioDevice, AudioSpecDesired, AudioSpecWAV};
-    use sdl2::Sdl;
-    use std::time::Duration;
+    use sdl2::audio::{AudioCVT, AudioCallback, AudioSpecDesired, AudioSpecWAV};
+use std::borrow::Cow;
+use std::path::{Path, PathBuf};
+use std::time::Duration;
 
-    pub struct CopiedData {
-        bytes: Vec<u8>,
-        position: usize,
-    }
-    impl AudioCallback for CopiedData {
-        type Channel = u8;
-        fn callback(&mut self, data: &mut [u8]) {
-            let (start, end) = (self.position, self.position + data.len());
-            self.position += data.len();
-            let audio_data = &self.bytes[start..end];
-            for (src, dst) in audio_data.iter().zip(data.iter_mut()) {
-                *dst = *src;
-            }
+// NOTE: You probably want to investigate the
+// mixer feature for real use cases.
+
+struct Sound {
+    data: Vec<u8>,
+    volume: f32,
+    pos: usize,
+}
+
+impl AudioCallback for Sound {
+    type Channel = u8;
+
+    fn callback(&mut self, out: &mut [u8]) {
+        for dst in out.iter_mut() {
+            // With channel type u8 the "silence" value is 128 (middle of the 0-2^8 range) so we need
+            // to both fill in the silence and scale the wav data accordingly. Filling the silence
+            // once the wav is finished is trivial, applying the volume is more tricky. We need to:
+            // * Change the range of the values from [0, 255] to [-128, 127] so we can multiply
+            // * Apply the volume by multiplying, this gives us range [-128*volume, 127*volume]
+            // * Move the resulting range to a range centered around the value 128, the final range
+            //   is [128 - 128*volume, 128 + 127*volume] – scaled and correctly positioned
+            //
+            // Using value 0 instead of 128 would result in clicking. Scaling by simply multiplying
+            // would not give correct results.
+            let pre_scale = *self.data.get(self.pos).unwrap_or(&128);
+            let scaled_signed_float = (pre_scale as f32 - 128.0) * self.volume;
+            let scaled = (scaled_signed_float + 128.0) as u8;
+            *dst = scaled;
+            self.pos += 1;
         }
     }
-    struct WrappedData {
-        audio: AudioSpecWAV,
-        position: usize,
-    }
-    impl AudioCallback for WrappedData {
-        type Channel = u8;
-        fn callback(&mut self, data: &mut [u8]) {
-            let (start, end) = (self.position, self.position + data.len());
-            self.position += data.len();
-            let audio_data = &self.audio.buffer()[start..end];
-            for (src, dst) in audio_data.iter().zip(data.iter_mut()) {
-                *dst = *src;
-            }
-        }
-    }
-    struct SquareWave {
-        phase_inc: f32,
-        phase: f32,
-        volume: f32,
-    }
+}
+    pub fn run() -> Result<(), String> {
+        // let path = format!("{}/assets/sounds/startup.wav", env!("CARGO_MANIFEST_DIR"));
+         let wav_file: Cow<'static, Path> = match std::env::args().nth(1) {
+        None => Cow::from(Path::new("/home/sna/Documents/clone/iced_custom_widget/assets/sounds/startup.wav")),
+        Some(s) => Cow::from(PathBuf::from(s)),
+    };
+    let sdl_context = sdl2::init()?;
+    let audio_subsystem = sdl_context.audio()?;
 
-    impl AudioCallback for SquareWave {
-        type Channel = f32;
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44_100),
+        channels: Some(1), // mono
+        samples: None,     // default
+    };
 
-        fn callback(&mut self, out: &mut [f32]) {
-            // Generate a square wave
-            for x in out.iter_mut() {
-                *x = if self.phase <= 0.8 {
-                    self.volume
-                } else {
-                    -self.volume
-                };
-                self.phase = (self.phase + self.phase_inc) % 1.0;
-            }
-        }
-    }
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        let wav = AudioSpecWAV::load_wav(wav_file).expect("Could not load test WAV file");
 
-    unsafe impl Send for WrappedData {}
-    pub fn run() {
-        let handler = std::thread::spawn(move || {
-            let sdl_context = sdl2::init().unwrap();
-            let audio_subsystem = sdl_context.audio().unwrap();
-            let desired_spec = AudioSpecDesired {
-                freq: Some(44100),
-                channels: Some(1), // mono
-                samples: None,     // default sample size
-            };
-            let device = audio_subsystem
-                .open_playback(None, &desired_spec, |spec| {
-                    // initialize the audio callback
-                    SquareWave {
-                        phase_inc: 440.0 / spec.freq as f32,
-                        phase: 0.0,
-                        volume: 0.25,
-                    }
-                })
-                .unwrap();
-            // Start playback
-            device.resume();
-            // Play for 2 seconds
-            // std::thread::sleep(Duration::from_millis(1000));
-        });
-        match handler.join() {
-            Ok(()) => println!("run success"),
-            Err(e) => println!("thread crashed: {:?} ", e),
+        let cvt = AudioCVT::new(
+            wav.format,
+            wav.channels,
+            wav.freq,
+            spec.format,
+            spec.channels,
+            spec.freq,
+        )
+        .expect("Could not convert WAV file");
+
+        let data = cvt.convert(wav.buffer().to_vec());
+
+        // initialize the audio callback
+        Sound {
+            data: data,
+            volume: 0.25,
+            pos: 0,
         }
+    })?;
+
+    // Start playback
+    device.resume();
+
+    // Play for a second
+    std::thread::sleep(Duration::from_millis(1_000));
+
+    // Device is automatically closed when dropped
+
+    Ok(())  
     }
 }
